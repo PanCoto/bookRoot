c65082dd7382a799a790684785fedbcb78f757f3
Bluzacy <91865@student.pb.edu.pl>
2026-05-26 00:52:27 +0200

    wstępna konfiguracja

---
+++ src/main/java/pl/studyshare/StudyShareApplication.java
@@ -0,0 +1,11 @@
+package pl.studyshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyShareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudyShareApplication.class, args);
+    }
+}
